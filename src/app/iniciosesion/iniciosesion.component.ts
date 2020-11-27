efaeb3c8eb8fce5bdbd89a35ec7d75f0e8b1cecb
sor7o <angelsorto330@gmail.com>
2020-11-27 15:53:24 -0600

    correccion inicio de sesion

--- src/app/iniciosesion/iniciosesion.component.ts
+++ src/app/iniciosesion/iniciosesion.component.ts
@@ -1,8 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, NgZone } from '@angular/core';
 import { of } from 'rxjs';
 import { User } from '../models/user';
 import { userService } from '../registrarse/services/userservices';
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
+
+
+import { FirebaseServiceService } from '../services/firebase-service.service';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-iniciosesion',
@@ -15,9 +19,14 @@ export class IniciosesionComponent implements OnInit {
   msg: string;
   msgAlert: boolean;
   @Input() users: User[];
+  email: string;
+  password: string;
   constructor(
     public userService: userService,
-    private router:Router
+    private router: Router,
+    public firebaseServiceService: FirebaseServiceService,
+    public afath: AngularFireAuth,
+    public ngZone: NgZone,
   ) { }
 
   ngOnInit(): void {
@@ -25,46 +34,35 @@ export class IniciosesionComponent implements OnInit {
     this.state = false;
     this.msgAlert = false;
     this.msg = '';
-  }
 
-  Login(email: HTMLInputElement, password: HTMLInputElement) {
-    console.log(email.value, password.value);
-    let tamsession = sessionStorage.length;
-    let finduser = this.users.filter(x => x.email === email.value).filter(y => y.password === password.value);
-
-    if (tamsession === 1) {
-      this.msg = "Ya iniciaste sesion"
-      this.msgAlert = true;
-      email.value = "";
-      password.value = "";
-      setTimeout('window.location.reload()', 1200);
-    } else {
-      if (finduser && finduser.length == 1) {
-        sessionStorage.setItem('currentUser', JSON.stringify({
-          token: 'jwt ', email: finduser[0].email, cod: finduser[0].id, name: finduser[0].name
-        }));
-        this.state = true;
-        this.msgAlert = false;
-        this.router.navigate([`Home`])
-        
-        setTimeout('window.location.reload()',0); 
-        return of(finduser[0]);
-      } else {
-        this.msgAlert = true;
-        this.msg = 'El usuario no existe o la contraseña es incorrecta';
-        setTimeout('window.location.reload()', 1500);
-        return of(null)
+    /* this.afath.user.subscribe(user => {
+      if (user) {
         
       }
-    }
-    return false;
+    }) */
   }
-  currentUser() {
-    return JSON.parse(sessionStorage.getItem('currentUser'));
+
+  Login() {
+    let em = (<HTMLInputElement>document.getElementById("email")).value;
+    let pass = (<HTMLInputElement>document.getElementById("password")).value;
+    if(em =="" || pass==""){
+      this.msg="Lo sentimos, debes de llenar todos los campos";
+      this.msgAlert=true;
+    }else{
+    this.afath.signInWithEmailAndPassword(this.email, this.password).then(()=>{
+      this.router.navigate(['/Home']);
+    }).catch(respo=>{  
+      this.msgAlert=true;
+      this.msg= respo.message
+    });
   }
-    
-  
-    
+  }
+  cerrar() {
+    this.msgAlert = false;
+  }
+
+
+
 
 
 }
