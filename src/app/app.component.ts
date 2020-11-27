151411b5a6027e69aa2134cefafcbe2906c9de54
sor7o <angelsorto330@gmail.com>
2020-11-27 15:53:40 -0600

    estado de sesion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
+import { FirebaseServiceService } from './services/firebase-service.service';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,20 +12,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent implements OnInit{
-  state:boolean;
+export class AppComponent implements OnInit {
+
   title = 'MusicLab';
   constructor(
-  ){}
-  ngOnInit(){
-    if(sessionStorage.length ===1){
-      console.log('here');  
-      this.state=true
-    }
+    private fbSS: FirebaseServiceService,
+    private afAuth: AngularFireAuth,
+    private router: Router,
+    private ngZone: NgZone,
 
+  ) { }
+  state: boolean;
+  name: string;
+  async ngOnInit() {
+    const user= await this.fbSS.getUserAtuh();
+    if(user){
+      this.state=false;
+      console.log('user=>', user);
+    }if(user ===null){
+      this.state=true;
+      console.log('not user', user);
+    }
+    
+    
+
+/* 
+    this.afAuth.onAuthStateChanged((user) => {
+      if (user) {
+        // User is signed in.
+        console.log(user);
+        this.state = false;
+      } else {
+        // No user is signed in.
+        console.log(user);
+
+        this.state = true;
+      }
+    }); */
   }
-  logout(){
-    this.state=false;
-    sessionStorage.removeItem('currentUser');  
+  logout() {
+    this.state = true;
+    this.fbSS.SingOut();
+    /* 
+    sessionStorage.removeItem('currentUser');   */
   }
 }
